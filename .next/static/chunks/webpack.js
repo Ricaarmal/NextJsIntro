f902e2ce8ff06a1a432dfb6b47fffa59e4cfef6a
Ricardo Manzano <noteplay101@hotmail.com>
2023-07-03 03:55:28 -0600

    Ruteo estatico y agregado de gitignore con node_modules

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "230e5624d0cab1b1"; }
+/******/ 		__webpack_require__.h = function() { return "5110b753b6cfede2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
